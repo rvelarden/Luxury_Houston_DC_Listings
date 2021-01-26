856f1ccbb7717201313e34779802e37b25f9b66f
Kishana Liburd <kishana.liburd@gmail.com>
2021-01-26 15:10:09 -0600

    third update

--- Backend_Luxury_Houston_DC_Listings /db/migrate/20210125220408_create_sellers.rb
+++ Backend_Luxury_Houston_DC_Listings /db/migrate/20210125220408_create_sellers.rb
@@ -1,6 +1,7 @@
 class CreateSellers < ActiveRecord::Migration[6.0]
   def change
     create_table :sellers do |t|
+      t.string :name
       t.string :telephone
       t.string :email_address
       t.string :occupation
---
+++ Backend_Luxury_Houston_DC_Listings /db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_25_220706) do
+
+  create_table "agents", force: :cascade do |t|
+    t.string "name"
+    t.string "telephone"
+    t.string "work_address"
+    t.string "email_address"
+    t.string "years_of_experience"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "address"
+    t.string "description"
+    t.string "number_of_beds"
+    t.string "number_of_baths"
+    t.integer "size"
+    t.string "price"
+    t.string "agent_id"
+    t.string "seller_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.string "rating"
+    t.string "agent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sellers", force: :cascade do |t|
+    t.string "name"
+    t.string "telephone"
+    t.string "email_address"
+    t.string "occupation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
